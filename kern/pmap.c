2573be675d1ac4db43ca1748e6535c78c49c3dc7
Julian Zhou <junzhou365@gmail.com>
2017-03-12 17:00:09 -0700

    half-way lab3

--- kern/pmap.c
+++ kern/pmap.c
@@ -162,6 +162,8 @@ mem_init(void)
 	//////////////////////////////////////////////////////////////////////
 	// Make 'envs' point to an array of size 'NENV' of 'struct Env'.
 	// LAB 3: Your code here.
+    envs = (struct Env *)boot_alloc(NENV * sizeof(struct Env));
+    memset(envs, 0, NENV * sizeof(struct Env));
 
 	//////////////////////////////////////////////////////////////////////
 	// Now that we've allocated the initial kernel data structures, we set
@@ -195,6 +197,8 @@ mem_init(void)
 	//    - the new image at UENVS  -- kernel R, user R
 	//    - envs itself -- kernel RW, user NONE
 	// LAB 3: Your code here.
+    boot_map_region(kern_pgdir, UENVS, PTSIZE, PADDR(envs), PTE_U);
+    assert(page_insert(kern_pgdir, pa2page(PADDR(envs)), envs, PTE_W) == 0);
 
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
@@ -413,8 +417,6 @@ boot_map_region(pde_t *pgdir, uintptr_t va, size_t size, physaddr_t pa, int perm
         // Find the corresponding page. If the page does not exist, create one.
         pte_t *entry = pgdir_walk(pgdir, (uintptr_t *) (va + offset), 1);
         assert(entry != NULL);
-        /*pgdir[PDX(va + offset)] &= ~0xFFF;*/
-        /*pgdir[PDX(va + offset)] |= perm | PTE_P;*/
         *entry = (pa + offset) | perm | PTE_P;
     }
 }
